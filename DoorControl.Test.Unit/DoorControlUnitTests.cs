ac00362b02fb77fa4abd15aa3c1bb15c49e5c5ed
mvraa <40834090+mvraa@users.noreply.github.com>
2020-09-24 13:27:17 +0200

    sdf

---
+++ DoorControl.Test.Unit/DoorControlUnitTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+
+//jenkins test2
+namespace DoorControl.Test.Unit
+{
+    [TestFixture]
+    public class DoorControlUnitTest
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+           
+        }
+
+        [Test]
+        public void Test()
+        {
+            Assert.That(1, Is.EqualTo(1));
+        }
+    }
+}
