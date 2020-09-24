204f35116b70aff7ec84d4f6c6334ac79b847cac
mvraa <40834090+mvraa@users.noreply.github.com>
2020-09-24 13:20:48 +0200

    door control start

---
+++ DoorControl/Application.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DoorControl
+{
+    public class Application
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Test");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ DoorControl/DoorControl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+//
+namespace DoorControl
+{
+    public class DoorControl
+    {
+        // private int _threshold;
+
+        public DoorControl()
+        {
+            
+        }
+    }
+}
